e5b5832734f3de1641b3c50053c2d5d57186ddaf
Rich Sharp <richpsharp@gmail.com>
2024-05-17 19:33:51 -0700

    better modeling

--- abstract_query_pipeline.py
+++ abstract_query_pipeline.py
@@ -7,12 +7,20 @@ import os
 import pickle
 import re
 import sys
+import textwrap
 
+from dotenv import load_dotenv
 from nltk.tokenize import sent_tokenize
+from transformers import pipeline
 import faiss
 import fitz
-import torch
 import spacy
+import torch
+import tiktoken
+
+#GPT_MODEL = 'gpt-4o'
+GPT_MODEL = 'gpt-3.5-turbo'
+ENCODING = tiktoken.encoding_for_model(GPT_MODEL)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -22,14 +30,17 @@ logging.basicConfig(
 logging.getLogger('taskgraph').setLevel(logging.INFO)
 logging.getLogger('sentence_transformers').setLevel(logging.WARN)
 logging.getLogger('PyPDF2._reader').setLevel(logging.ERROR)
+logging.getLogger('httpx').setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-#GPT_MODEL = 'gpt-4o' # 'gpt-3.5-turbo'
-#ENCODING = tiktoken.encoding_for_model(GPT_MODEL)
-TOP_K = 10
+#GPT_MODEL = 'gpt-4o'
+GPT_MODEL = 'gpt-3.5-turbo'
+TOP_K = 100
 
 # how big of a window to build around sentences
-SLIDING_WINDOW_SIZE = 10
+SLIDING_WINDOW_SENTENCE_SIZE = 10
+SENTENCE_WINDOW_OVERLAP = 2
+SLIDING_WINDOW_CHARACTER_SIZE = 500
 BODY_TAG = 'body'
 CITATION_TAG = 'citation'
 
@@ -42,6 +53,15 @@ for dirpath in [CACHE_DIR]:
     os.makedirs(dirpath, exist_ok=True)
 
 
+def trim_conversation(context, max_tokens=4096):
+    # Tokenizer should be used to count tokens accurately; this is a simplified example
+    total_tokens = len(ENCODING.encode(context))
+    while total_tokens > max_tokens:
+        overshoot = max_tokens - total_tokens
+        context = ' '.join(context.split()[:-overshoot//2])
+        print(len(context))
+        total_tokens = len(ENCODING.encode(context))
+    return context
 
 # Example function to concatenate document contents and generate a hash
 def generate_hash(documents):
@@ -62,24 +82,26 @@ def extract_text_from_pdf(pdf_path):
             print(f'processing page {page_num} {os.path.basename(pdf_path)}')
             raw_text = page.get_text()
             clean_text = raw_text.replace(' \n', ' ').replace('\n', ' ')
-            sentence_windows = split_into_sentence_windows(clean_text)
-            if page_num > 5:
-                break
+            sentence_windows = split_into_sentence_windows(page_num, clean_text)
             sentence_windows_per_page.append((page_num, sentence_windows))
     return sentence_windows_per_page
 
 
-def split_into_sentence_windows(text):
+def split_into_sentence_windows(page_num, text):
     doc = nlp(text)
     sentences = [sent.text for sent in doc.sents]
-    current_offset = 0
-    sentence_window = []
-    while current_offset < len(sentences):
-        sentence_window.append(' '.join(sentences[
-            current_offset:current_offset + SLIDING_WINDOW_SIZE]))
-
-        current_offset += SLIDING_WINDOW_SIZE - 1
-    return sentence_window
+    current_sentence_offset = 0
+    sentence_windows = []
+    while current_sentence_offset < len(sentences):
+        working_window = ' '.join(sentences[current_sentence_offset:current_sentence_offset + SLIDING_WINDOW_SENTENCE_SIZE])
+        current_sentence_offset += SLIDING_WINDOW_SENTENCE_SIZE
+        while len(working_window) < SLIDING_WINDOW_CHARACTER_SIZE and current_sentence_offset < len(sentences):
+            working_window += ' ' + sentences[current_sentence_offset]
+            current_sentence_offset += 1
+        sentence_windows.append(working_window)
+        if current_sentence_offset != len(sentences):
+            current_sentence_offset -= SENTENCE_WINDOW_OVERLAP
+    return sentence_windows
 
 
 def parse_file(file_path):
@@ -111,11 +133,30 @@ def main():
     expanded_sentence_window_path = os.path.join(CACHE_DIR, f'{file_hash}.pkl')
     fiass_path = os.path.join(CACHE_DIR, f'{file_hash}.faiss')
 
-    if False and os.path.exists(expanded_sentence_window_path):
+    from sentence_transformers import SentenceTransformer
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    embedding_model = SentenceTransformer(
+        'sentence-transformers/all-MiniLM-L6-v2').to(device)
+    qa_model_list = []
+    for model_id in [
+            #'FacebookAI/xlm-roberta-large',
+            #'timpal0l/mdeberta-v3-base-squad2',
+            #'deepset/bert-large-uncased-whole-word-masking-squad2',
+            #'distilbert/distilbert-base-cased-distilled-squad',
+            #'google-bert/bert-large-uncased-whole-word-masking-finetuned-squad',
+            #'allenai/scibert_scivocab_uncased',
+            'deepset/roberta-base-squad2',
+            ]:
+        qa_model_list.append((
+            model_id,
+            None))
+
+    if os.path.exists(expanded_sentence_window_path):
         with open(expanded_sentence_window_path, 'rb') as file:
             expanded_sentence_windows = pickle.load(file)
         file_path_list, page_num_list, sentence_windows = zip(
             *expanded_sentence_windows)
+        document_distance_index = faiss.read_index(fiass_path)
     else:
         article_list = []
         for file_path in file_paths:
@@ -130,15 +171,11 @@ def main():
             for page_num, sentence_windows in sentence_windows_per_page
             for sentence_window in sentence_windows]
         with open(expanded_sentence_window_path, 'wb') as file:
-            pickle.dump(article_list, file)
+            pickle.dump(expanded_sentence_windows, file)
         file_path_list, page_num_list, sentence_windows = zip(
             *expanded_sentence_windows)
 
         LOGGER.debug('embedding')
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        from sentence_transformers import SentenceTransformer
-        embedding_model = SentenceTransformer(
-            'sentence-transformers/all-MiniLM-L6-v2').to(device)
         document_embeddings = embedding_model.encode(
             sentence_windows, convert_to_tensor=True)
         LOGGER.debug('indexing')
@@ -157,26 +194,111 @@ def main():
         # Retrieve the most similar documents
         distances, indices = document_distance_index.search(
             question_embedding, TOP_K)
-        relevent_text = [sentence_windows[idx] for idx in indices[0]]
-        relevent_page_numbers = [page_num_list[idx] for idx in indices[0]]
+        print(f'****************** {distances}, {indices}')
+        retrieved_windows = [sentence_windows[idx] for idx in indices[0]]
+        relevant_page_numbers = [page_num_list[idx] for idx in indices[0]]
         relevant_files = [file_path_list[idx] for idx in indices[0]]
 
-        return '\n   * '.join([
-            f'{file_path}: {page_num} - \n\t{" ".join(sentence_window)}'
-            for file_path, page_num, sentence_window in
-            zip(relevent_text,
-                relevent_page_numbers,
-                relevant_files)])
+        answers = []
+        # for context, page_number, filename, score in zip(
+        #         retrieved_windows, relevant_page_numbers, relevant_files, distances[0]):
+        #     answers.append((context, score, page_number, filename))
+
+
+        for qa_model_id, qa_model in qa_model_list:
+            print(qa_model_id)
+            qa_model = pipeline('question-answering', model=model_id, device=device)
+            for context, page_number, filename in zip(
+                    retrieved_windows, relevant_page_numbers, relevant_files):
+                answer = qa_model(question=question, context=context)
+                answers.append((answer['answer'], answer['score'], qa_model_id, context, page_number, filename))
+            del qa_model
+
+        response = question
+        for answer, score, qa_model_id, context, page_number, filename in sorted(
+                answers, key=lambda x: x[1], reverse=False):
+            response += (
+                f'\n\n  * "{question}"?\n\tAnswer: "{answer}" ({score:.3f} (out of 1.0) - {qa_model_id})'
+                f'\n\t  -- context from page {page_number} of "{filename}":'
+                f'\n\t\t{context}')
+        return response
+
+    def answer_question_with_gpt(question):
+        # Encode the question
+        from openai import OpenAI
+        client = OpenAI()
+
+        question_embedding = embedding_model.encode(
+            question, convert_to_tensor=True).cpu().numpy()
+
+        # Ensure the question_embedding is 2D
+        if len(question_embedding.shape) == 1:
+            question_embedding = question_embedding.reshape(1, -1)
+
+        distances, indices = document_distance_index.search(
+            question_embedding, TOP_K)
+
+        retrieved_windows = [sentence_windows[idx] for idx in indices[0]]
+        relevant_page_numbers = [page_num_list[idx] for idx in indices[0]]
+        relevant_files = [file_path_list[idx] for idx in indices[0]]
+
+        # Concatenate the retrieved documents to form the context
+        context = " ".join([
+            f'reference index: {index}", context: {context}\n\n'
+            for index, (filename, page_number, context) in enumerate(zip(
+                relevant_files, relevant_page_numbers, retrieved_windows))])
+
+        context = trim_conversation(context, max_tokens=20000)
+        context_counts = context.count("context: ")
+        stream = client.chat.completions.create(
+            model=GPT_MODEL,
+            messages=[
+                {"role": "system", "content": "You are given a set of filename/page number/text snippets. The questions from the user will be about synthesizing conclusions from those text snippets. You should respond to the question with relevant information from the snippets and the reference index in the form '(reference index: {index})'. If you do not have enough information to answer, say so. Do not make up any information."},
+                {"role": "user", "content": f"Context: {context}\n\nQuestion: {question}\nAnswer:"}
+            ],
+            stream=True,
+            max_tokens=4000  # Adjust the number of tokens to get a longer answer
+        )
+        response = f'From {context_counts} references to the texts -- '
+        for chunk in stream:
+            if chunk.choices[0].delta.content is not None:
+                response += chunk.choices[0].delta.content
+        index_matches = re.findall(r'\(reference index: \{?(\d+)\}?\)', response)
+        wrapper = textwrap.TextWrapper(
+            width=80,
+            initial_indent='    ',  # Initial indent for the first line
+            subsequent_indent='    '  # Indent for all subsequent lines
+        )
+        wrapped_response = wrapper.fill(response)
+
+        wrapper = textwrap.TextWrapper(
+            width=70,
+            initial_indent='       ',  # Initial indent for the first line
+            subsequent_indent='       '  # Indent for all subsequent lines
+        )
+
+        processed_indexes = set()
+        for index in sorted(index_matches):
+            index = int(index)
+            if index in processed_indexes:
+                continue
+            processed_indexes.add(index)
+            wrapped_response += '\n\n' + wrapper.fill(
+                f'reference index {index}. "{relevant_files[index]}" pg{relevant_page_numbers[index]}: '
+                f'{retrieved_windows[index]}')
+        return wrapped_response
 
     while True:
-        question = input("Enter your question (or type 'exit' to quit): ")
+        question = input("\nEnter your question (or type 'exit' to exit): ")
         if question.lower() == 'exit':
             break
         if question.strip() == '':
             continue
-        response = query_index(question)
-        print(f"\n\n{response}")
+        #response = query_index(question)
+        response = answer_question_with_gpt(question)
+        print('Answer:\n' + response)
 
 
 if __name__ == '__main__':
+    load_dotenv()
     main()
