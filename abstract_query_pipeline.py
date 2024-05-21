e81e27e85fd22eed461de7e4e2ad6afba2ebacd7
Rich Sharp <richpsharp@gmail.com>
2024-05-20 20:09:15 -0700

    better trimming of tokens

--- abstract_query_pipeline.py
+++ abstract_query_pipeline.py
@@ -11,12 +11,15 @@ import textwrap
 
 from dotenv import load_dotenv
 from nltk.tokenize import sent_tokenize
+from openai import OpenAI
 from transformers import pipeline
 import faiss
 import fitz
+import openai
 import spacy
-import torch
 import tiktoken
+import torch
+
 
 GPT_MODEL = 'gpt-4o'
 #GPT_MODEL = 'gpt-3.5-turbo'
@@ -56,11 +59,12 @@ for dirpath in [CACHE_DIR]:
 def trim_conversation(context, max_tokens=4096):
     # Tokenizer should be used to count tokens accurately; this is a simplified example
     total_tokens = len(ENCODING.encode(context))
+    print(f'starting token count: {total_tokens} (max allowed {max_tokens})')
     while total_tokens > max_tokens:
         overshoot = max_tokens - total_tokens
         context = ' '.join(context.split()[:-overshoot//2])
-        print(len(context))
         total_tokens = len(ENCODING.encode(context))
+    print(f'trimmed token count: {total_tokens}')
     return context
 
 # Example function to concatenate document contents and generate a hash
@@ -220,73 +224,77 @@ def main():
 
     def answer_question_with_gpt(question):
         # Encode the question
-        from openai import OpenAI
-        client = OpenAI()
+        try:
+            question_embedding = embedding_model.encode(
+                question, convert_to_tensor=True).cpu().numpy()
 
-        question_embedding = embedding_model.encode(
-            question, convert_to_tensor=True).cpu().numpy()
+            # Ensure the question_embedding is 2D
+            if len(question_embedding.shape) == 1:
+                question_embedding = question_embedding.reshape(1, -1)
 
-        # Ensure the question_embedding is 2D
-        if len(question_embedding.shape) == 1:
-            question_embedding = question_embedding.reshape(1, -1)
+            distances, indices = document_distance_index.search(
+                question_embedding, TOP_K)
 
-        distances, indices = document_distance_index.search(
-            question_embedding, TOP_K)
+            retrieved_windows = [sentence_windows[idx] for idx in indices[0]]
+            relevant_page_numbers = [page_num_list[idx] for idx in indices[0]]
+            relevant_files = [file_path_list[idx] for idx in indices[0]]
 
-        retrieved_windows = [sentence_windows[idx] for idx in indices[0]]
-        relevant_page_numbers = [page_num_list[idx] for idx in indices[0]]
-        relevant_files = [file_path_list[idx] for idx in indices[0]]
+            # Concatenate the retrieved documents to form the context
+            context = " ".join([
+                f'reference index: {index}", context: {context}\n\n'
+                for index, (filename, page_number, context) in enumerate(zip(
+                    relevant_files, relevant_page_numbers, retrieved_windows))])
 
-        # Concatenate the retrieved documents to form the context
-        context = " ".join([
-            f'reference index: {index}", context: {context}\n\n'
-            for index, (filename, page_number, context) in enumerate(zip(
-                relevant_files, relevant_page_numbers, retrieved_windows))])
+            context = trim_conversation(context, max_tokens=20000)
+            context_counts = context.count("context: ")
+            stream = OPENAI_CLIENT.chat.completions.create(
+                model=GPT_MODEL,
+                messages=[
+                    {"role": "system", "content": "You are given a set of filename/page number/text snippets. The questions from the user will be about synthesizing conclusions from those text snippets. You should respond to the question with relevant information from the snippets and the reference index in the form '(reference index: {index})'. If you do not have enough information to answer, say so. Do not make up any information."},
+                    {"role": "user", "content": f"Context: {context}\n\nQuestion: {question}\nAnswer:"}
+                ],
+                stream=True,
+                max_tokens=4000  # Adjust the number of tokens to get a longer answer
+            )
+            response = f'From {context_counts} references to the texts -- '
+            for chunk in stream:
+                if chunk.choices[0].delta.content is not None:
+                    response += chunk.choices[0].delta.content
+            index_matches = re.findall(
+                r'\(reference index: \{?([\d,\s]+?)\}?\)', response)
+            parsed_indexes = set(
+                [int(num) for match in index_matches
+                 for num in re.split(r'[\s,]+', match.strip())])
 
-        context = trim_conversation(context, max_tokens=20000)
-        context_counts = context.count("context: ")
-        stream = client.chat.completions.create(
-            model=GPT_MODEL,
-            messages=[
-                {"role": "system", "content": "You are given a set of filename/page number/text snippets. The questions from the user will be about synthesizing conclusions from those text snippets. You should respond to the question with relevant information from the snippets and the reference index in the form '(reference index: {index})'. If you do not have enough information to answer, say so. Do not make up any information."},
-                {"role": "user", "content": f"Context: {context}\n\nQuestion: {question}\nAnswer:"}
-            ],
-            stream=True,
-            max_tokens=4000  # Adjust the number of tokens to get a longer answer
-        )
-        response = f'From {context_counts} references to the texts -- '
-        for chunk in stream:
-            if chunk.choices[0].delta.content is not None:
-                response += chunk.choices[0].delta.content
-        index_matches = re.findall(r'\(reference index: \{?(\d+(?:, \d+)*?)\}?\)', response)
-        parsed_indexes = set([
-            int(num) for match in index_matches for num in match.split(', ')])
-        wrapper = textwrap.TextWrapper(
-            width=80,
-            initial_indent='    ',  # Initial indent for the first line
-            subsequent_indent='    '  # Indent for all subsequent lines
-        )
-        wrapped_response = wrapper.fill(response)
+            wrapper = textwrap.TextWrapper(
+                width=80,
+                initial_indent='    ',  # Initial indent for the first line
+                subsequent_indent='    '  # Indent for all subsequent lines
+            )
+            wrapped_response = wrapper.fill(response)
 
-        wrapper = textwrap.TextWrapper(
-            width=70,
-            initial_indent='       ',  # Initial indent for the first line
-            subsequent_indent='       '  # Indent for all subsequent lines
-        )
+            wrapper = textwrap.TextWrapper(
+                width=70,
+                initial_indent='       ',  # Initial indent for the first line
+                subsequent_indent='       '  # Indent for all subsequent lines
+            )
 
-        processed_indexes = set()
-        for index in sorted(parsed_indexes):
-            index = int(index)
-            if index in processed_indexes:
-                continue
-            processed_indexes.add(index)
-            wrapped_response += '\n\n' + wrapper.fill(
-                f'reference index {index}. "{relevant_files[index]}" pg{relevant_page_numbers[index]}: '
-                f'{retrieved_windows[index]}')
-        return wrapped_response
+            processed_indexes = set()
+            for index in sorted(parsed_indexes):
+                index = int(index)
+                if index in processed_indexes:
+                    continue
+                processed_indexes.add(index)
+                wrapped_response += '\n\n' + wrapper.fill(
+                    f'reference index {index}. "{relevant_files[index]}" pg{relevant_page_numbers[index]}: '
+                    f'{retrieved_windows[index]}')
+            return wrapped_response
+        except openai.APIError as e:
+            print(f'There was an error, try your question again.\nError: {e}')
 
     while True:
-        question = input("\nEnter your question (or type 'exit' to exit): ")
+        question = input(
+            "\nEnter your question (or type 'exit' to exit): ").strip()
         if question.lower() == 'exit':
             break
         if question.strip() == '':
@@ -298,4 +306,5 @@ def main():
 
 if __name__ == '__main__':
     load_dotenv()
+    OPENAI_CLIENT = OpenAI()
     main()
